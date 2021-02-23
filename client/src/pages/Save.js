73abe09f56449b123a5237a2b0f8c5515add3e67
graciela23garcia <70188756+graciela23garcia@users.noreply.github.com>
2021-02-22 21:21:05 -0800

    created function to delete books

---
+++ client/src/pages/Save.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Button, Container } from "react-bootstrap";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+import DeleteBtn from "../components/DeleteBtn";
+
+
+function Books() {
+  // Setting our component's initial state
+  const [books, setBooks] = useState([])
+  const [formObject, setFormObject] = useState({})
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
+  // Loads all books and sets them to books
+  function loadBooks() {
+    API.getBooks()
+      .then(res => 
+        setBooks(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  // Deletes a book from the database with a given id, then reloads books from the db
+  function deleteBook(id) {
+    API.deleteBook(id)
+      .then(res => loadBooks())
+      .catch(err => console.log(err));
+  }
+
+  // Handles updating component state when the user types into the input field
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  };
+
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Books On My List</h1>
+            </Jumbotron>
+            {books.length ? (
+              <List>
+                {books.map(book => (
+                  <ListItem key={book._id}>
+                    
+                      <strong>
+                        {book.title} by {book.authors}
+                      </strong>
+                      <p>{book.description}</p>
+                      <img src={book.image}></img>
+                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                    <Button href={book.link}>View Book</Button>
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
+
+export default Books;
